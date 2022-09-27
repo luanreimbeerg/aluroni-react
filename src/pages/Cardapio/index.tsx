594fd20afa9ec9563fb6344c09435987d918d8c9
Luan Trajano <luan.trajano@biz.com.br>
2022-09-26 23:01:27 -0300

    feat: adjusting design styles and created cardapio component

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,3 +1,12 @@
+import style from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from "assets/logo.svg";
+
 export default function Cardapio() {
-  return <main></main>;
+  return (
+    <main>
+      <nav className={style.menu}>
+        <Logo />
+      </nav>
+    </main>
+  );
 }
